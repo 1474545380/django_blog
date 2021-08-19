65220b0b1c8f5f30812cd74e6314ea4e9219ad94
liulongxin <1474545380@qq.com>
2021-08-19 23:18:31 +0800

    定义写文章的模型类

--- new_pro/home/models.py
+++ new_pro/home/models.py
@@ -1,5 +1,17 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+#写文章
 class ArticleCategory(models.Model):
-    pass
+    #标题
+    title=models.CharField(max_length=100,blank=True)
+    #分类的创建时间
+    created=models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return self.title
+    class Meta:
+        #修改表名
+        db_table='tb_category'
+        #admin站点显示，方便查看对象
+        verbose_name='类别管理'
+        verbose_name_plural=verbose_name
